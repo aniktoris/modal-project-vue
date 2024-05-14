58b2d0f2dcf02f894f26c21c22b974d7754931fa
oleno <es.moname@gmail.com>
2024-05-14 10:30:28 +0200

    add modal component and events emitting

---
+++ src/components/Modal.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{sale: theme === 'sale'}">
+      <h1>{{header}}</h1>
+      <p>{{text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['header', 'text', 'theme'],
+  methods: {
+    closeModal(){
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.modal{
+  background: white;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  background: rgba(0,0,0,0.5);
+  width: 100%;
+  height: 100%;
+  position: fixed;
+}
+
+h1{
+  color: orange;
+  border: none;
+  padding: 0;
+}
+
+.sale {
+  background: crimson;
+  color: white;
+}
+
+.sale h1 {
+  color: white;
+}
+
+</style>
